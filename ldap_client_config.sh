3300bb2342154f9e237b0996c863af79eec878d0
rmahmood19 <reaz.u.mahmood@gmail.com>
2016-12-13 23:03:07 +0100

    Update ldap_client_config.sh

--- ldap_client_config.sh
+++ ldap_client_config.sh
@@ -6,18 +6,20 @@
 # LICENCE:  Copyleft free software
 #
 # Variables
-# Replace the value with your Openldap server URL
 LD_SERVER=0
-# Replace the value with your Openldap Basedn
+
 LD_BASEDN=0
 #replace the vakue with your CA certificate URL
 LD_CACERT=0
 
 yum install -y openldap-clients nss-pam-ldapd
+# This will prompt the user for Openldap server URL
 echo -n "Enter Ldap Server Name: > "
 read LD_SERVER
+# This will prompt the user for Openldap base Dn
 echo -n "Enter Ldap Basedn: > "
 read LD_BASEDN
+# This will prompt the user for Url to CA certificate
 echo -n "Enter URL for downloading CA certificate: > "
 read LD_CACERT
 authconfig --enableldap --enableldapauth --ldapserver=$LD_SERVER --ldapbasedn=$LD_BASEDN --ldaploadcacert=$LD_CACERT --enableldaptls --update
